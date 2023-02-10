3399ff72f410d81b0335841a7c428e70d4cde37a
ericaYingk <62686741+yingkli@users.noreply.github.com>
2023-02-10 15:16:08 -0800

    assign8

--- client_example.py
+++ client_example.py
@@ -1,7 +1,7 @@
 #
-#   Hello World client in Python
-#   Connects REQ socket to tcp://localhost:5555
-#   Sends "A message from CS381" to server, expects "Received by server" back
+#   dictTolist client in Python
+#   Connects REQ socket to tcp://localhost:5560
+#   Sends activity dictionary to server, expects "Received by server" back
 #
 
 import zmq
@@ -11,14 +11,13 @@ context = zmq.Context()
 #  Socket to talk to server
 print("Connecting to server…")
 socket = context.socket(zmq.REQ)
-socket.connect("tcp://localhost:5555")
+socket.connect("tcp://localhost:5560")
 
-print("Sending message...")
-dict = {"Exercise":[["run", 30],["swim", 60]], "Work":[["Email", 90], ["Meetings", 120]], "Academics" : []}
+# sending request (sample activity dictionary) to server
+print("Sending activity dictionary...")
+activity_dict = {"Exercise":[["run", 30],["swim", 60]], "Work":[["Email", 90], ["Meetings", 120]], "Academics" : []}
+socket.send_json(activity_dict)
 
-socket.send_json(dict)
-
-message = socket.recv_json()
-
-print(type(message))
-print(message)
+# receive reply from server
+activity_list = socket.recv_json()
+print(f"Received reply from server\n{activity_list}")
--- dictTOlist_server.py
+++ dictTOlist_server.py
@@ -1,36 +1,47 @@
 #
-#   Hello World server in Python
-#   Binds REP socket to tcp://*:5555
-#   Expects "A message from CS381 " from client, replies with "Received by server"
-#
+#   dictTolist server in Python
+#   Binds REP socket to tcp://*:5560
+#   Expects an activity dictionary from client, replies with a list with activity category and total time spent
+#   Example format of dictionary: 
+#   {"Exercise":[["run", 30],["swim", 60]], "Work":[["Email", 90], ["Meetings", 120]], "Academics" : []}
+#   Example format of list reply:
+#   [["Exercise", "Work", "Academics"],[90, 210, 0]]
+
+
 
 import time
 import zmq
 import json
 
+# binds socket
 context = zmq.Context()
 socket = context.socket(zmq.REP)
-socket.bind("tcp://*:5555")
+socket.bind("tcp://*:5560")
 
 while True:
     #  Wait for next request from client
-    message = socket.recv_json()
-    print(f"Received message from client:\n{message}")
-    #dict = json.loads(message)
-    print(type(message))
-    #  Do some 'work'
+    activity_dict = socket.recv_json()
+    print(f"Received message from client:\n{activity_dict}")
+ 
     time.sleep(1)
     
+    # create reply list
+    activity = []
+    # create sublists in reply list
     activity_list = []
     activity_time = []
-    for key in message:
+    # find all activity categories (keys) in the dictionary
+    for key in activity_dict:
         activity_list.append(key)
         total_time = 0
-        for item in message[key]:
+        # for each activity category (key), sub total time spent for it
+        for item in activity_dict[key]:
             total_time += item[1]
         activity_time.append(total_time)
-    activity = []
+    # append sublists to the reply list
     activity.append(activity_list)
     activity.append(activity_time)
-        #  Send reply back to client
+    
+    #  Send reply back to client
+    print("Sending reply to client...")
     socket.send_json(activity)
